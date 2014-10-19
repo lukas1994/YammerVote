8668e856e41848cc35881addee0c84f166923560
Lukas Köbis <lukaskoebis@gmail.com>
2014-10-19 08:58:08 +0100

    hjdshk

--- app.js
+++ app.js
@@ -70,9 +70,7 @@ var yam = new Yammer({ access_token: "M0OrJhe9QWxYMCkATSkvNA"});
 
  app.get('/userpolls', function(req, res){
    Poll.find({user_id: req.query.user_id}, function(err, data){
-     return res.jsonp(
-       data
-     );
+     return res.jsonp(data);
    });
  });
 
@@ -99,7 +97,7 @@ var yam = new Yammer({ access_token: "M0OrJhe9QWxYMCkATSkvNA"});
    if (!('group' in req.query)) {
      req.query.group = '';
    }
-   Poll.findOneRandom(function(err, poll){
+   Poll.findOneRandom(/*{user_id: {$ne: req.query.user_id}},*/function(err, poll){
      if (err) {
    		console.log(err);
    	  }
